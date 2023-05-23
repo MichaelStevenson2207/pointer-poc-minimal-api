beaaa12fa75f9ac8bbc22ed546a56412bf775d5f
MichaelStevenson2207 <wubblyjuggly@hotmail.co.uk>
2023-05-23 21:14:42 +0100

    always use swagger

--- pointer-poc-minimal-api/Program.cs
+++ pointer-poc-minimal-api/Program.cs
@@ -82,11 +82,8 @@ builder.Services.AddSwaggerGen(options =>
 
 var app = builder.Build();
 
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
 
